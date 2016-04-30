2343a25e43cb09ab807f42e6edac19282603a4fb
doktor_justice <somiandras@gmail.com>
2016-04-30 17:43:12 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name euro2016App
+ * @description
+ * # euro2016App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('euro2016App', [
+    'ngAnimate',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize'
+  ]);
