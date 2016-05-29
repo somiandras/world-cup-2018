6719c07b56bc25caa3b0222072b39137b76c25c4
doktor_justice <somiandras@gmail.com>
2016-05-29 13:45:10 +0200

    Add welcome modal to dashboard if user has no username yet

--- app/scripts/dashboard/dashboard.controller.js
+++ app/scripts/dashboard/dashboard.controller.js
@@ -4,14 +4,42 @@
 
 	angular.module('appCore').controller('DashboardController', DashboardController);
 
-	DashboardController.$inject = ['tournamentService', 'user'];
+	DashboardController.$inject = ['tournamentService', 'user', '$uibModal'];
 
-	function DashboardController (tournamentService, user) {
+	function DashboardController (tournamentService, user, $uibModal) {
 
 		let vm = this;
 
 		vm.tour = tournamentService;
 		vm.user = user;
+
+		if (!user.name) {
+
+			let modalInstance = $uibModal.open({
+				templateUrl: 'views/welcome_modal.html',
+				controller: 'modalController',
+				controllerAs: 'modal',
+				animation: true,
+				backdrop: 'static',
+				keyboard: false,
+				resolve: {
+					user: function () {
+
+						return user;
+					}
+				}
+			});
+
+			modalInstance.result.then((result) => {
+
+				toastr.success(result);
+			})
+			.catch((error) => {
+
+				toastr.error(error);
+			})
+		}
+
 	}
 	
 })();
---
+++ app/scripts/dashboard/modal.controller.js
@@ -0,0 +1,37 @@
+(function () {
+
+	'use strict';
+
+	angular.module('appCore').controller('modalController', modalController);
+
+	modalController.$inject = ['$uibModalInstance', 'userService', 'user'];
+
+	function modalController ($uibModalInstance, userService, user) {
+
+		let vm = this;
+		vm.user = user;
+
+		vm.saveUser = function (name, company) {
+
+			if (!name) {
+
+				throw new Error('Nincs név megadva');
+			}
+
+			user.name = name;
+
+			if (company) {
+
+				user.company = company;
+			}
+
+			userService.saveUser(user)
+			.then((resp) => {
+
+				$uibModalInstance.close('Elmentettük az adataidat!');
+			})
+		}
+
+	}
+
+})();
