18f3850a1c2de36ad834e2cc39d26deee16d8080
doktor_justice <somiandras@gmail.com>
2016-06-09 18:48:22 +0200

    Hide favourites on public profile until the start of tournament

--- app/scripts/user/public.controller.js
+++ app/scripts/user/public.controller.js
@@ -4,15 +4,17 @@
 
 	angular.module('user').controller('PublicController', PublicController);
 
-	PublicController.$inject = ['currentUser', 'userService', 'tournamentService'];
+	PublicController.$inject = ['currentUser', 'userService', 'tournamentService', 'APP_CONFIG'];
 
-	function PublicController (currentUser, userService, tournamentService) {
+	function PublicController (currentUser, userService, tournamentService, APP_CONFIG) {
 
 		let vm = this;
 		let tour = tournamentService;
 
 		vm.user = currentUser;
 		vm.matches = tour.data.matches;
+		vm.now = new Date().getTime();
+		vm.start = APP_CONFIG.startTime;
 	}
 
 })();
