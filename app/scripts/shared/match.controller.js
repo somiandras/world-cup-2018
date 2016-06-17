38be80c2e81a2b674b2393f8039b3a15d8b2b685
doktor_justice <somiandras@gmail.com>
2016-06-17 20:57:45 +0200

    Sort by points on match results page

--- app/scripts/shared/match.controller.js
+++ app/scripts/shared/match.controller.js
@@ -9,13 +9,42 @@
 	function MatchController (match, user, userService) {
 
 		let vm = this;
-		vm.users = userService.public;
 		vm.current = match;
 		vm.user = user;
+		vm.users = userService.public;
+
+		vm.userList = getUserList(match);
 
 		if (user.league && user.league.length) {
 
 			vm.leagueFilter = user.league[0];
+		}
+
+		function getUserList (match) {
+
+			let prepArray = vm.users.map((thisUser) => {
+
+				let prepUser = {};
+				prepUser.name = thisUser.name;
+				prepUser.league = thisUser.league;
+				prepUser.uid = thisUser.uid;
+
+				if (thisUser.bets && thisUser.bets.matches[match.$id]) {
+
+					prepUser.home = thisUser.bets.matches[match.$id].home;
+					prepUser.away = thisUser.bets.matches[match.$id].away;
+					prepUser.points = thisUser.bets.matches[match.$id].points;
+
+				} else {
+
+					prepUser.points = 0;
+
+				}
+
+				return prepUser;
+			});
+
+			return prepArray;
 		}
 	}
 
